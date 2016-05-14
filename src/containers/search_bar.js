4aad7def25b4f7f43e01a3341be730221aa2cd91
kcvan <kcongvan@gmail.com>
2016-05-14 15:25:53 -0700

    Add comments, and handle onSubmit whenever user presses enter or submit button

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -19,9 +19,27 @@ export default class SearchBar extends Component {
     console.log(event.target.value);
     this.setState({term:event.target.value});
   }
+
+  onFormSubmit(event) {
+    // This will tell the browser to not submit the form itself
+    event.preventDefault();
+  }
+
+  // When you press enter ony our keyboard while a form element child is focused (search bar or submit button),
+  // the form automatically thinks you're trying to submit the contents of this form. So when we have
+  // our cursor inside the input, click the button, or press enter, the browser thinks youre trying ot submit
+  // a html form, so it sends a post request to the server.
+
+  // Since we don't want the page to refresh every time someone presses enter or tries to submit, we can add a
+  // function handler (native react attribute onSubmit) that we can pass in a callback that we create in the class.
+
+  // If we're using preventDefault, why do we use a form anyway? We use it because we get some things for free.
+  // We get the behavior for typing into a input and clicking submit or pressing enter will submit our form for us.
+  // If we didn't use form, we would have to set event listeners for the enter key and submit click. Now we only have
+  // one event handler, our onSubmit.
   render() {
     return (
-        <form className="input-group">
+        <form onSubmit={this.onFormSubmit} className="input-group">
           <input 
             placeholder="Get a five-day forcast in your favorite cities"
             className="form-control"
