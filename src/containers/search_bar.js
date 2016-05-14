34be078e86782bd12a7e55c29adf2073d18ee102
kcvan <kcongvan@gmail.com>
2016-05-14 12:21:03 -0700

    Add comments on binding and this keyword

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -12,6 +12,8 @@ export default class SearchBar extends Component {
     this.onInputChange = this.onInputChange.bind(this);
   }
   
+  // You cant use a fat arrow function inside a class, so you ether write the callback out 
+  // in the event handler, or you bind it in the constructor.
   onInputChange(event) {
     console.log(event.target.value);
     this.setState({term:event.target.value});
