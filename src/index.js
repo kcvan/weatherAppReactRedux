69195bc13b03714080134a3e33fcf46fe8dedd5d
kcvan <kcongvan@gmail.com>
2016-05-14 17:23:02 -0700

    Add middleware import statement and add middleware to our applyMiddleware function with ReduxPromise. Also obtain API key

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// Importing our redux-promise middleware package
+import ReduxPromise from "redux-promise";
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// We add the middleware inside the applyMiddleware function.
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
